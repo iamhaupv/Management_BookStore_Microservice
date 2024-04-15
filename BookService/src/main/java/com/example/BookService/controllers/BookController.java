749062069802bd72a2f742743774363e83440e65
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-04-15 17:52:07 +0700

    deleteBookById successfully

--- BookService/src/main/java/com/example/BookService/controllers/BookController.java
+++ BookService/src/main/java/com/example/BookService/controllers/BookController.java
@@ -3,7 +3,9 @@ package com.example.BookService.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,5 +28,11 @@ public class BookController {
 	@PostMapping("/book")
 	public Book addBook(@RequestBody Book book) {
 		return service.addBook(book);
+	}
+	// delete book by id
+	@DeleteMapping("/{id}")
+	public String deleteBookById(@PathVariable Integer id) {
+		service.deleteBookById(id);
+		return "delete book successfully!";
 	}
 }
