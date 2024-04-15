de50cd488f5f749652e968279706de8c74ae527a
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-04-15 19:34:29 +0700

    updateUser successfully

--- UserService/src/main/java/com/example/UserService/controllers/UserController.java
+++ UserService/src/main/java/com/example/UserService/controllers/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -38,5 +39,10 @@ public class UserController {
 	@DeleteMapping("/user/{id}")
 	public String deleteUserById(@PathVariable Integer id) {
 		return service.deleteUserById(id);
+	}
+	// update user
+	@PatchMapping("/user/{id}")
+	public User updateUser(@PathVariable Integer id, @RequestBody User user) {
+		return service.updateUser(id, user);
 	}
 }
