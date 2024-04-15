49864bef2508b7fec343e9d5251d3680d13d8ca7
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-04-15 17:30:00 +0700

    getAllBooks successfully!

--- BookService/src/main/java/com/example/BookService/controllers/BookController.java
+++ BookService/src/main/java/com/example/BookService/controllers/BookController.java
@@ -1,14 +1,22 @@
 package com.example.BookService.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.BookService.models.Book;
+import com.example.BookService.service.BookService;
+
 @RestController
 @RequestMapping("/api/v1")
 public class BookController {
+	@Autowired
+	private BookService service;
 	@GetMapping("/books")
-	public String getAllBooks() {
-		return "books";
+	public List<Book> getAllBooks() {
+		return service.getAllBooks();
 	}
 }
