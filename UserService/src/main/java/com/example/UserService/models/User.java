b74615f7b53b864889cec7aadae76280c7e0ec55
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-04-15 18:53:08 +0700

    models create table users successfully

---
+++ UserService/src/main/java/com/example/UserService/models/User.java
@@ -0,0 +1,47 @@
+package com.example.UserService.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+	@Id
+	@GeneratedValue
+	private int id;
+	private String username;
+	private String password;
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public User(int id, String username, String password) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + "]";
+	}
+}
