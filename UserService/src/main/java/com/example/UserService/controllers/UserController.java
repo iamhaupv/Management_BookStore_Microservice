db1e64ce8b08e793109e5c6069760542a8e47b86
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-04-15 19:23:31 +0700

    getUserById successfully

--- UserService/src/main/java/com/example/UserService/controllers/UserController.java
+++ UserService/src/main/java/com/example/UserService/controllers/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,5 +27,10 @@ public class UserController {
 	@PostMapping("/user")
 	public User addUser(@RequestBody User user) {
 		return service.addUser(user);
+	}
+	// get user by id
+	@GetMapping("/{id}")
+	public User getUserById(@PathVariable Integer id) {
+		return service.getUerById(id);
 	}
 }
