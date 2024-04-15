c71df9f50036d0da46eb78c7d91ea650fcc50d7b
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-04-15 16:58:51 +0700

    api/v1/books

--- BookService/src/main/java/com/example/BookService/controllers/BookController.java
+++ BookService/src/main/java/com/example/BookService/controllers/BookController.java
@@ -1,5 +1,14 @@
 package com.example.BookService.controllers;
 
-public class BookController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/v1")
+public class BookController {
+	@GetMapping("/books")
+	public String getAllBooks() {
+		return "books";
+	}
 }
