4cd85ce445425211857546643ef08faa3e4b0dba
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-04-15 16:46:52 +0700

    update pom.xml bookservice

---
+++ BookService/src/main/java/com/example/BookService/controllers/BookController.java
@@ -0,0 +1,5 @@
+package com.example.BookService.controllers;
+
+public class BookController {
+
+}
---
+++ BookService/src/main/java/com/example/BookService/models/Book.java
@@ -0,0 +1,56 @@
+package com.example.BookService.models;
+
+public class Book {
+	private int id;
+	private String name;
+	private String author;
+	public String publisher;
+	private int publishingYear;
+	public Book() {
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", name=" + name + ", author=" + author + ", publisher=" + publisher
+				+ ", publishingYear=" + publishingYear + "]";
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	public int getPublishingYear() {
+		return publishingYear;
+	}
+	public void setPublishingYear(int publishingYear) {
+		this.publishingYear = publishingYear;
+	}
+	public Book(int id, String name, String author, String publisher, int publishingYear) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.author = author;
+		this.publisher = publisher;
+		this.publishingYear = publishingYear;
+	}
+	
+}
