8c5b0ba2dde17e5a488c940abe0730906bc99cb7
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-04-15 17:45:36 +0700

    addBook successfully!

--- BookService/src/main/java/com/example/BookService/controllers/BookController.java
+++ BookService/src/main/java/com/example/BookService/controllers/BookController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +17,14 @@ import com.example.BookService.service.BookService;
 public class BookController {
 	@Autowired
 	private BookService service;
+	// get all books
 	@GetMapping("/books")
 	public List<Book> getAllBooks() {
 		return service.getAllBooks();
+	}
+	// add book
+	@PostMapping("/book")
+	public Book addBook(@RequestBody Book book) {
+		return service.addBook(book);
 	}
 }
--- BookService/src/main/java/com/example/BookService/models/Book.java
+++ BookService/src/main/java/com/example/BookService/models/Book.java
@@ -2,7 +2,6 @@ package com.example.BookService.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -10,7 +9,7 @@ import jakarta.persistence.Table;
 @Table(name="books")
 public class Book {
 	@Id
-	@GeneratedValue(strategy =  GenerationType.AUTO)
+	@GeneratedValue
 	private int id;
 	private String name;
 	private String author;
