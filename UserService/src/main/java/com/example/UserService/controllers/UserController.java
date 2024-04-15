749f9f152590d98de2ac612cb4a9e2e1b7168203
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-04-15 19:27:16 +0700

    deleteUserById successfully

--- UserService/src/main/java/com/example/UserService/controllers/UserController.java
+++ UserService/src/main/java/com/example/UserService/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.example.UserService.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,5 +33,10 @@ public class UserController {
 	@GetMapping("/{id}")
 	public User getUserById(@PathVariable Integer id) {
 		return service.getUerById(id);
+	}
+	// delete user by id
+	@DeleteMapping("/user/{id}")
+	public String deleteUserById(@PathVariable Integer id) {
+		return service.deleteUserById(id);
 	}
 }
