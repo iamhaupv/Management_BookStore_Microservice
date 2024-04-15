63d2084dfb241834f2114e778a14e7f491ba470d
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-04-15 17:24:48 +0700

    entity book successfully!

--- BookService/src/main/java/com/example/BookService/models/Book.java
+++ BookService/src/main/java/com/example/BookService/models/Book.java
@@ -1,6 +1,16 @@
 package com.example.BookService.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="books")
 public class Book {
+	@Id
+	@GeneratedValue(strategy =  GenerationType.AUTO)
 	private int id;
 	private String name;
 	private String author;
