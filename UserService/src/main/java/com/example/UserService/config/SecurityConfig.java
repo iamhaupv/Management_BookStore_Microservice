30ca9cef8d964e832120a5330dfc70fb72148b5d
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-04-16 14:16:45 +0700

    update pom.xml

---
+++ UserService/src/main/java/com/example/UserService/config/SecurityConfig.java
@@ -0,0 +1,7 @@
+package com.example.UserService.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
