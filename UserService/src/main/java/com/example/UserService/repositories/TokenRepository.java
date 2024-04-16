09015819ea9f9f9c4745f747b3ce95ecff25f4da
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-04-16 13:58:28 +0700

    TokenRepository successfully

--- UserService/src/main/java/com/example/UserService/repositories/TokenRepository.java
+++ UserService/src/main/java/com/example/UserService/repositories/TokenRepository.java
@@ -3,10 +3,8 @@ package com.example.UserService.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.UserService.models.User;
-
-
+import ch.qos.logback.core.subst.Token;
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>{
-	User findByUsername(String username);
+public interface TokenRepository extends JpaRepository<Token, Long>{
+	 Token findByToken(String token);
 }
