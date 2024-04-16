f3b64df8911125868af0861664db805c15f90354
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-04-16 17:23:47 +0700

    UserService successfully

---
+++ UserService/src/main/java/com/example/spring_boot_api_jwt_ad/service/UserService.java
@@ -0,0 +1,16 @@
+package com.example.spring_boot_api_jwt_ad.service;
+
+import com.example.spring_boot_api_jwt_ad.authen.UserPrincipal;
+import com.example.spring_boot_api_jwt_ad.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+    public List<User> getAllUsers();
+    public User addUser(User user);
+    public User getUerById(Long id);
+    public User updateUser(Long id, User user);
+    public String deleteUserById(Long id);
+    User createUser(User user);
+    UserPrincipal findByUsername(String username);
+}
