f627d875d11e554edeeefa946e7f1c85494b9b9f
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-04-20 15:38:48 +0700

    BookService add Entity Category successfully

--- BookService/src/main/java/com/example/BookService/models/Book.java
+++ BookService/src/main/java/com/example/BookService/models/Book.java
@@ -5,10 +5,15 @@ import java.util.Date;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
@@ -36,7 +41,10 @@ public class Book implements Serializable{
 	private Integer quantity;
 	private String des;
 	private Double discount;
-
+	@ManyToOne
+	@JoinColumn(name = "category_id")
+	@JsonIgnore
+	private Category category;
 	public Book() {
 		// TODO Auto-generated constructor stub
 	}
---
+++ BookService/src/main/java/com/example/BookService/models/Category.java
@@ -0,0 +1,53 @@
+package com.example.BookService.models;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "category_id")
+	private Integer id;
+	private String name;
+	@OneToMany(mappedBy = "category")
+	@JsonIgnore
+	private List<Book> books;
+	public Category() {
+		// TODO Auto-generated constructor stub
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Category [id=" + id + ", name=" + name + "]";
+	}
+	public Category(Integer id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	
+}
