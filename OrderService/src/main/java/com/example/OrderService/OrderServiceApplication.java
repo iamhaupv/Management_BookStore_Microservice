85f2b6ebaa3fd2f3ab88e647d4432f4d5708606a
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-04-21 15:04:58 +0700

    catche successfully

--- OrderService/src/main/java/com/example/OrderService/OrderServiceApplication.java
+++ OrderService/src/main/java/com/example/OrderService/OrderServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.OrderService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
--- OrderService/src/main/java/com/example/OrderService/services/CartServiceImpl.java
+++ OrderService/src/main/java/com/example/OrderService/services/CartServiceImpl.java
@@ -5,17 +5,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.OrderService.dto.BookDTO;
+
 @Service
 public class CartServiceImpl implements CartService {
 	@Autowired
 	private RestTemplate restTemplate;
 	public Map<Integer, BookDTO> map = new HashMap<>();
+	private static final String CART_CACHE_NAME = "cart";
+
 	// add book to cart call BookService api
+	@CachePut(value = CART_CACHE_NAME)
 	public void addBookToCart(Integer id) {
 		BookDTO p = map.get(id);
 		String url = "http://localhost:8081/api/v1/book/" + id;
@@ -53,17 +60,21 @@ public class CartServiceImpl implements CartService {
 
 	// items
 	@Override
+	@Cacheable(value = CART_CACHE_NAME)
 	public Collection<BookDTO> getItems() {
 		return map.values();
 	}
 
 	// remove from cart
 	@Override
+	@CacheEvict(value = CART_CACHE_NAME, allEntries = true)
 	public void removeBookFromCart(Integer id) {
 		map.remove(id);
 	}
+
 	// clear cart
 	@Override
+	@CacheEvict(value = CART_CACHE_NAME, allEntries = true)
 	public void clearCart() {
 		map.clear();
 	}
