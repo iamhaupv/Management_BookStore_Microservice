7eba756b34cd5d6163958b165db73c2cba5a61d7
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-04-27 12:01:07 +0700

    api gateway bookservice-get-books successfully

--- APIGateway/src/main/java/com/example/APIGateway/ApiGatewayApplication.java
+++ APIGateway/src/main/java/com/example/APIGateway/ApiGatewayApplication.java
@@ -1,9 +1,11 @@
 package com.example.APIGateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
