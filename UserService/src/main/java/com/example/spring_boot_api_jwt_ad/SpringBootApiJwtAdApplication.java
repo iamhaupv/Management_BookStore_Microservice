051395ff135c4f3bf4c6f8617b428dbf9fffb5e2
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-04-29 17:13:02 +0700

    discovery successfully

--- UserService/src/main/java/com/example/spring_boot_api_jwt_ad/SpringBootApiJwtAdApplication.java
+++ UserService/src/main/java/com/example/spring_boot_api_jwt_ad/SpringBootApiJwtAdApplication.java
@@ -3,9 +3,9 @@ package com.example.spring_boot_api_jwt_ad;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
 @SpringBootApplication
 @EnableJpaAuditing
+
 public class SpringBootApiJwtAdApplication {
 
     public static void main(String[] args) {
