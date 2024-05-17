a6e7d7c6cc41b1424875296b272faa98576b2b7b
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-05-17 17:07:14 +0700

    update discovery

--- DiscoveryService/src/main/java/com/example/DiscoveryService/DiscoveryServiceApplication.java
+++ DiscoveryService/src/main/java/com/example/DiscoveryService/DiscoveryServiceApplication.java
@@ -3,9 +3,8 @@ package com.example.DiscoveryService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
-@SpringBootApplication
 @EnableEurekaServer
+@SpringBootApplication
 public class DiscoveryServiceApplication {
 
 	public static void main(String[] args) {
