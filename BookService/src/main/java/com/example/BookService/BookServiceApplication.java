41d1339ee18093e5f1d906614a96e3ba06ebb224
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-05-19 14:06:45 +0700

    docker-compose book, discovery, apigateway

--- BookService/src/main/java/com/example/BookService/BookServiceApplication.java
+++ BookService/src/main/java/com/example/BookService/BookServiceApplication.java
@@ -3,9 +3,11 @@ package com.example.BookService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableCaching
+@EnableDiscoveryClient
 public class BookServiceApplication {
 
 	public static void main(String[] args) {
