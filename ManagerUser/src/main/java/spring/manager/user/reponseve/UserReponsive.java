ec0230c9f8bd650db50c035b7b08569a65692c62
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-05-25 13:13:01 +0700

    up

--- ManagerUser/src/main/java/spring/manager/user/reponseve/UserReponsive.java
+++
@@ -1,9 +0,0 @@
-package spring.manager.user.reponseve;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import spring.manager.user.entity.User;
-
-public interface UserReponsive extends JpaRepository<User, Integer> {
-
-}
